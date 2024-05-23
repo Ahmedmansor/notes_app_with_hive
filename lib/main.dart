fe81666e5f8ca3bece2933afd74396199f500232
Ahmedmansor <conantotta@gmail.com>
2024-05-23 22:29:56 +0300

    create NotesAppWithHive

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const NotesAppWithHive());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class NotesAppWithHive extends StatelessWidget {
+  const NotesAppWithHive({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
