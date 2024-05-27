7412b3d0395dab879688b6108cfa30dbd614b374
Ahmedmansor <conantotta@gmail.com>
2024-05-27 07:09:44 +0300

    Add modal progress HUD and update add note bottom sheet
    - Add modal progress HUD to show loading state when adding a note
    - Update add note bottom sheet to use BlocConsumer and AddNoteCubit
    - Remove AddNoteForm widget and move its contents to AddNoteBottomSheet
    - Add AddNoteBottomSheetForm as a new file (currently empty)
    - Add modal_progress_hud_nsn package to pubspec.yaml
    - Update hive and hive_flutter packages to latest versions

--- lib/core/widgets/add_note_bottom_sheet.dart
+++ lib/core/widgets/add_note_bottom_sheet.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app_with_hive/colors.dart';
 import 'package:notes_app_with_hive/core/utils/app_settings.dart';
+import 'package:notes_app_with_hive/core/utils/navigation.dart';
 import 'package:notes_app_with_hive/core/widgets/custom_button.dart';
+import 'package:notes_app_with_hive/cubits/add_note_cubit.dart/cubit/add_note_cubit.dart';
 import 'package:notes_app_with_hive/cubits/cubit/main_cubit.dart';
 
+import 'add_note_bottom_sheet_form.dart';
 import 'custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -11,77 +16,25 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
-      ),
-    );
-  }
-}
-
-class AddNoteForm extends StatelessWidget {
-  const AddNoteForm({
-    super.key,
-  });
-  @override
-  Widget build(BuildContext context) {
-    var mainCubit = MainCubit.get(context);
-
-    return Form(
-      key: mainCubit.formKey,
-      autovalidateMode: mainCubit.autovalidateMode,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 36,
-          ),
-          CustomTextField(
-            title: 'title',
-            onSaved: (value) {
-              mainCubit.title = value;
-            },
-            validator: (p0) {
-              if (p0?.isEmpty ?? true) {
-                return 'Please enter title';
-              }
-              return null;
-            },
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            title: 'content',
-            maxLines: 5,
-            onSaved: (p0) {
-              mainCubit.content = p0;
-            },
-            validator: (p0) {
-              if (p0?.isEmpty ?? true) {
-                return 'Please enter content';
-              }
-              return null;
-            },
-          ),
-          SizedBox(
-            height: AppSettings.height * 0.1,
-          ),
-          SizedBox(
-            width: AppSettings.width,
-            child: CustomButton(
-              backgroundColor: ColorsApp.kPrimaryColor,
-              onPressed: () {
-                mainCubit.validate();
-              },
-              text: 'Add',
-              textColor: Colors.black,
-            ),
-          ),
-          SizedBox(
-            height: AppSettings.height * 0.02,
-          ),
-        ],
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSucess) {
+              NavigationUtils.offScreen(context);
+            }
+            if (state is AddNoteError) {
+              debugPrint('error ${state.error}');
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
---
+++ lib/core/widgets/add_note_bottom_sheet_form.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app_with_hive/colors.dart';
+import 'package:notes_app_with_hive/core/widgets/custom_text_field.dart';
+import 'package:notes_app_with_hive/cubits/cubit/main_cubit.dart';
+
+import '../utils/app_settings.dart';
+import 'custom_button.dart';
+
+class AddNoteForm extends StatelessWidget {
+  const AddNoteForm({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    var mainCubit = MainCubit.get(context);
+
+    return Form(
+      key: mainCubit.formKey,
+      autovalidateMode: mainCubit.autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 36,
+          ),
+          CustomTextField(
+            title: 'title',
+            onSaved: (value) {
+              mainCubit.title = value;
+            },
+            validator: (p0) {
+              if (p0?.isEmpty ?? true) {
+                return 'Please enter title';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            title: 'content',
+            maxLines: 5,
+            onSaved: (p0) {
+              mainCubit.content = p0;
+            },
+            validator: (p0) {
+              if (p0?.isEmpty ?? true) {
+                return 'Please enter content';
+              }
+              return null;
+            },
+          ),
+          SizedBox(
+            height: AppSettings.height * 0.1,
+          ),
+          SizedBox(
+            width: AppSettings.width,
+            child: CustomButton(
+              backgroundColor: ColorsApp.kPrimaryColor,
+              onPressed: () {
+                mainCubit.validate();
+              },
+              text: 'Add',
+              textColor: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: AppSettings.height * 0.02,
+          ),
+        ],
+      ),
+    );
+  }
+}
